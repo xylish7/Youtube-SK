a5cbcdebb45e1e87d78158346a3d8b6b3347d95a
xylish7 <filipfrincu@gmail.com>
2020-01-08 12:45:22 +0200

    Add custom titlebar

--- src/main/main.ts
+++ src/main/main.ts
@@ -19,7 +19,7 @@ const createWindow = async () => {
     await installExtensions();
   }
 
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({ width: 800, height: 600, resizable: false, frame: false });
 
   if (process.env.NODE_ENV !== 'production') {
     process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1';
--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,13 +1,27 @@
+import React, { useEffect } from 'react';
 import { hot } from 'react-hot-loader/root';
-import React from 'react';
+import { Titlebar, Color } from 'custom-electron-titlebar';
 
 import styles from './Application.css';
 import Home from './Home/Home';
 
-const Application = () => (
-  <div className={styles.container}>
-    <Home />
-  </div>
-);
+const Application = () => {
+  useEffect(() => {
+    const titlebar = new Titlebar({
+      backgroundColor: Color.fromHex('#e8e8e8'),
+      maximizable: false
+    });
+
+    return () => {
+      titlebar.dispose();
+    };
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Home />
+    </div>
+  );
+};
 
 export default hot(Application);
