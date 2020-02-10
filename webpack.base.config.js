79876ddc061a7c62b5930d044ee5ed626d6db131
xylish7 <filip.frincu@gmail.com>
2020-02-10 23:38:34 +0200

    Test electron updater

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath: '/'
+    // publicPath: '/'
   },
   node: {
     __dirname: false,
